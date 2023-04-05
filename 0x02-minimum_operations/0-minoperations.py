812d6d3eb53d829e5a7d69d4b9a71ccaf192ff70
brayohmurithidev <murithibrianm@gmail.com>
2023-04-05 22:44:08 +0300

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+'''
+A function to return minimum number of operation to carry out
+copy all and paste operations
+'''
+
+
+def minOperations(n):
+    '''
+    The function takes the least common factor of the given number
+    '''
+    factors = []
+    divisor = 2
+    while (n >= 2):
+        # check if n is divisible by 2
+        if (n % divisor == 0):
+            factors.append(divisor)
+            n /= divisor
+        else:
+            divisor += 1
+    return sum(factors)
