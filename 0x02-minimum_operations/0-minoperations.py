e0553fadf565d6c86845c8d9062289e26addd857
brayohmurithidev <murithibrianm@gmail.com>
2023-04-05 22:47:08 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,6 +10,8 @@ def minOperations(n):
     '''
     The function takes the least common factor of the given number
     '''
+    if(n < 2): 
+        return 0
     factors = []
     divisor = 2
     while (n >= 2):
