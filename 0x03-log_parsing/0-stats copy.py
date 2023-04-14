743a54ce5e44fb4c6863956b5ed7b12aed67766f
brayohmurithidev <murithibrianm@gmail.com>
2023-04-14 22:14:20 +0300

    updates

--- 0x03-log_parsing/0-stats copy.py
+++ 0x03-log_parsing/0-stats copy.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 
 """
-This module reads input from standard input and looks for lines that match a specific pattern.
+This module reads input from standard input and looks for lines that match
+a specific pattern.
 
-The pattern is defined as a regular expression in the variable `format_pattern`. Each line of
-input is checked against the pattern, and if it matches, the status code and total size are
-extracted and added to `status_codes` and `total_sizes`, respectively.
+The pattern is defined as a regular expression in the variable `format_pattern`.
+Each line of input is checked against the pattern, and if it matches, the status
+code and total size are extracted and added to `status_codes` and `total_sizes`,
+respectively.
 
-Every 10 input lines, the function `display_output()` is called to output the total file size and
-a count of occurrences for each status code.
+Every 10 input lines, the function `display_output()` is called to output the
+total file size and a count of occurrences for each status code.
 
-To use this module, simply pipe input into the script and it will automatically process the input
-and output the results.
+To use this module, simply pipe input into the script and it will automatically
+process the input and output the results.
 """
 
 import re
 import sys
 
-def displayOutput(statuses, sizes):
+def display_output(statuses, sizes):
     '''The function outputs size and status occurrences count'''
     sizes = [int(i) for i in sizes]
     sizes_sum = sum(sizes)
@@ -27,8 +29,10 @@ def displayOutput(statuses, sizes):
         occurence = statuses.count(status)
         print(f'{status}: {occurence}')
 
-format_pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '\
-    r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
+format_pattern = (
+    r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/\d+ HTTP/1\.1" '
+    r'(\d+) (\d+)$'
+)
 
 names = []
 input_count = 0
@@ -52,5 +56,3 @@ try:
 except KeyboardInterrupt:
     display_output(status_codes, total_sizes)
     raise
-
-
