414e9a73961b336198f33739a47ec7ff55b38ced
brayohmurithidev <murithibrianm@gmail.com>
2023-04-14 20:47:01 +0300

    added a main fn

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -18,51 +18,56 @@ import re
 import sys
 
 
-format_pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '\
-   r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
+def mainFun():
+    '''MAIN FUNCTION'''
 
-names = []
-input_count = 0
-total_sizes = []
-status_codes = []
+    format_pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '\
+    r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
 
-
-def check_pattern_match(pattern, userInput):
-    '''
-    The function checks user input against the pattern
-    '''
-    match = re.match(pattern, userInput)
-    if not match:
-        print('did not meet pattern')
-        return False
-    else:
-        total_size = match.group(4)
-        total_sizes.append(total_size)
-        status_code = match.group(3)
-        status_codes.append(status_code)
+    names = []
+    input_count = 0
+    total_sizes = []
+    status_codes = []
 
 
-def display_output(statuses, sizes):
-    '''The function outputs size and status occurrences count'''
-    sizes = [int(i) for i in sizes]
-    sizes_sum = sum(sizes)
-    print(f"File size: {sizes_sum}")
-    my_set = set(statuses)
-    for status in sorted(my_set):
-        occurence = statuses.count(status)
-        print(f'{status}: {occurence}')
+    def check_pattern_match(pattern, userInput):
+        '''
+        The function checks user input against the pattern
+        '''
+        match = re.match(pattern, userInput)
+        if not match:
+            print('did not meet pattern')
+            return False
+        else:
+            total_size = match.group(4)
+            total_sizes.append(total_size)
+            status_code = match.group(3)
+            status_codes.append(status_code)
 
 
-'''The code sets a condition that needs to be met.'''
-try:
-    while True:
-        name = sys.stdin.readline()
-        match = check_pattern_match(format_pattern, name)
-        if match is not False:
-            names.append(name)
-            input_count += 1
-            if input_count % 10 == 0:
-                display_output(status_codes, total_sizes)
+    def display_output(statuses, sizes):
+        '''The function outputs size and status occurrences count'''
+        sizes = [int(i) for i in sizes]
+        sizes_sum = sum(sizes)
+        print(f"File size: {sizes_sum}")
+        my_set = set(statuses)
+        for status in sorted(my_set):
+            occurence = statuses.count(status)
+            print(f'{status}: {occurence}')
 
-except KeyboardInterrupt:
-    display_output(status_codes, total_sizes)
+
+    '''The code sets a condition that needs to be met.'''
+    try:
+        while True:
+            name = sys.stdin.readline()
+            match = check_pattern_match(format_pattern, name)
+            if match is not False:
+                names.append(name)
+                input_count += 1
+                if input_count % 10 == 0:
+                    display_output(status_codes, total_sizes)
+
+    except KeyboardInterrupt:
+        display_output(status_codes, total_sizes)
+
+mainFun()
