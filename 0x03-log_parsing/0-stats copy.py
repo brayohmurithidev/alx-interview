81c704612053cc7ab0e8ea190efbec4bdbd9f6b6
brayohmurithidev <murithibrianm@gmail.com>
2023-04-14 22:07:57 +0300

    changes

---
+++ 0x03-log_parsing/0-stats copy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+"""
+This module reads input from standard input and looks for lines that match a specific pattern.
+
+The pattern is defined as a regular expression in the variable `format_pattern`. Each line of
+input is checked against the pattern, and if it matches, the status code and total size are
+extracted and added to `status_codes` and `total_sizes`, respectively.
+
+Every 10 input lines, the function `display_output()` is called to output the total file size and
+a count of occurrences for each status code.
+
+To use this module, simply pipe input into the script and it will automatically process the input
+and output the results.
+"""
+
+import re
+import sys
+
+def displayOutput(statuses, sizes):
+    '''The function outputs size and status occurrences count'''
+    sizes = [int(i) for i in sizes]
+    sizes_sum = sum(sizes)
+    print(f"File size: {sizes_sum}")
+    my_set = set(statuses)
+    for status in sorted(my_set):
+        occurence = statuses.count(status)
+        print(f'{status}: {occurence}')
+
+format_pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] '\
+    r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
+
+names = []
+input_count = 0
+total_sizes = []
+status_codes = []
+
+try:
+    while True:
+        name = sys.stdin.readline()
+        match = re.match(format_pattern, name)
+        if match:
+            names.append(name)
+            total_size = match.group(4)
+            total_sizes.append(total_size)
+            status_code = match.group(3)
+            status_codes.append(status_code)
+            input_count += 1
+            if input_count % 10 == 0:
+                display_output(status_codes, total_sizes)
+
+except KeyboardInterrupt:
+    display_output(status_codes, total_sizes)
+    raise
+
+
--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
 
-"""
-This module reads input from standard input and looks for lines that match a specific pattern.
+'''
+program
+'''
 
-The pattern is defined as a regular expression in the variable `format_pattern`. Each line of
-input is checked against the pattern, and if it matches, the status code and total size are
-extracted and added to `status_codes` and `total_sizes`, respectively.
-
-Every 10 input lines, the function `display_output()` is called to output the total file size and
-a count of occurrences for each status code.
-
-To use this module, simply pipe input into the script and it will automatically process the input
-and output the results.
-"""
-
-import re
-import sys
 
 def displayOutput(statuses, sizes):
-    '''
-    The function outputs size and status occurrences count
-    '''
+    '''The function outputs size and status occurrences count'''
     sizes = [int(i) for i in sizes]
     sizes_sum = sum(sizes)
     print(f"File size: {sizes_sum}")
@@ -28,5 +14,3 @@ def displayOutput(statuses, sizes):
     for status in sorted(my_set):
         occurence = statuses.count(status)
         print(f'{status}: {occurence}')
-
-
