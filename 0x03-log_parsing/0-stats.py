a1e3f2a9370ca881d1ae4d918268141fc610004b
brayohmurithidev <murithibrianm@gmail.com>
2023-04-14 08:59:26 +0300

    documentation revised

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 
-"""Program that takes in stdin and reads it line
-by line.
+"""
+This module reads input from standard input and looks for lines that match a specific pattern.
 
-Keyword arguments:
-pattern -- takes in the pattern string
-user_input -- takes in the user input string
-Return: returns a boleen incase of a match either true or false
+The pattern is defined as a regular expression in the variable `format_pattern`. Each line of
+input is checked against the pattern, and if it matches, the status code and total size are
+extracted and added to `status_codes` and `total_sizes`, respectively.
+
+Every 10 input lines, the function `display_output()` is called to output the total file size and
+a count of occurrences for each status code.
+
+To use this module, simply pipe input into the script and it will automatically process the input
+and output the results.
 """
 
 import re
