dd2efec3d67347fd7323ddaf74459174bd70f5c5
brayohmurithidev <murithibrianm@gmail.com>
2023-04-20 20:43:09 +0300

    updates

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python3
 
-"""This script reads input from standard input line by
-line and computes some statistics"""
+""" This script reads input from standard input line by
+line and computes some statistics """
 
 import sys
 
 
-def print_statistics(status_codes, total_size):
-    """Prints statistics for the input data"""
-    print("Total file size: {:d}".format(total_size))
-    for status, count in sorted(status_codes.items()):
-        if count != 0:
-            print("{}: {:d}".format(status, count))
+def printsts(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                "404": 0, "405": 0, "500": 0}
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
 
 count = 0
-total_size = 0
+size = 0
 
 try:
     for line in sys.stdin:
         if count != 0 and count % 10 == 0:
-            print_statistics(status_codes, total_size)
+            printsts(sts, size)
 
-        fields = line.split()
+        stlist = line.split()
         count += 1
 
         try:
-            size = int(fields[-1])
-            total_size += size
-        except ValueError:
+            size += int(stlist[-1])
+        except:
             pass
 
         try:
-            status_code = fields[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
-        except IndexError:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
             pass
+    printsts(sts, size)
 
-    print_statistics(status_codes, total_size)
 
 except KeyboardInterrupt:
-    print_statistics(status_codes, total_size)
+    printsts(sts, size)
     raise
